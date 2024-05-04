f056a7ced97bd82b6f84584615e59f5b7a46a5bc
felipe.prog.al@gmail.com <felipe.prog.al@gmail.com>
2024-05-04 11:40:47 -0300

    Modificações na classe do banco de dados, coisas que eu aprendi rescentemente.

--- banco_de_dados.py
+++ banco_de_dados.py
@@ -3,13 +3,14 @@ from datetime import datetime
 
 
 class Banco:
-    # Cria arquivo
-    banco = sqlite3.connect('dados_das_pessoas.db')
-    # Para utilizar os comandos sql
-    cursor = banco.cursor()
+    def __init__(self):
+        self.db_name = 'dados_das_pessoas.db'
 
     def carregar_dados(self, tabel, inicio=0, variavel_id=None, variavel_nome=None,
                        variavel_email=None, variavel_data=None):
+        banco = sqlite3.connect(self.db_name)
+        cursor = banco.cursor()
+
         # Inicia a consulta SQL
         consulta_sql = "SELECT id, nome, email, data FROM dados WHERE 1=1"
 
@@ -24,10 +25,10 @@ class Banco:
             consulta_sql += f" AND data = '{variavel_data}'"
 
         # Execute a consulta SELECT para recuperar os dados
-        self.cursor.execute(consulta_sql)
+        cursor.execute(consulta_sql)
 
         # Recupere todos os resultados
-        resultados = self.cursor.fetchall()
+        resultados = cursor.fetchall()
 
         max_rows = 21
 
@@ -48,40 +49,58 @@ class Banco:
             # Insere cada linha na tabela
             tabel.insert('', 'end', values=(id_linha, nome, email, data_formatada))
 
+        banco.close()
+
     def adicionar_dados(self,  __nome: str, __email: str, __data: datetime):
+        banco = sqlite3.connect(self.db_name)
+        cursor = banco.cursor()
+
         # Converte o objeto datetime para uma string no formato 'YYYY-mm-dd'
         data_final_str = __data.strftime('%Y-%m-%d')
 
         # Inserir dados no banco
-        self.cursor.execute("INSERT INTO dados (nome, email, data) VALUES (?, ?, ?)", (__nome, __email, data_final_str))
-        self.banco.commit()
+        cursor.execute("INSERT INTO dados (nome, email, data) VALUES (?, ?, ?)", (__nome, __email, data_final_str))
+        banco.commit()
+        banco.close()
 
     def editar_dados(self, id_alvo, novo_nome=None, novo_email=None, nova_data=None):
+        banco = sqlite3.connect(self.db_name)
+        cursor = banco.cursor()
+
         # Se novo_nome não for None, atualize o nome
         if novo_nome is not None:
-            self.cursor.execute(""" UPDATE dados SET nome = ? WHERE id = ? """, (novo_nome, id_alvo))
+            cursor.execute(""" UPDATE dados SET nome = ? WHERE id = ? """, (novo_nome, id_alvo))
 
         # Se novo_email não for None, atualize o email
         if novo_email is not None:
-            self.cursor.execute(""" UPDATE dados SET email = ? WHERE id = ? """, (novo_email, id_alvo))
+            cursor.execute(""" UPDATE dados SET email = ? WHERE id = ? """, (novo_email, id_alvo))
 
         # Se nova_data não for None, atualize a data
         if nova_data is not None:
-            self.cursor.execute(""" UPDATE dados SET data = ? WHERE id = ? """, (nova_data, id_alvo))
+            cursor.execute(""" UPDATE dados SET data = ? WHERE id = ? """, (nova_data, id_alvo))
 
         # Commit as mudanças no banco de dados
-        self.banco.commit()
+        banco.commit()
+        banco.close()
 
     def deletar_dados(self, __id):
+        banco = sqlite3.connect(self.db_name)
+        cursor = banco.cursor()
+
         try:
             # Deletar dados no banco
-            self.cursor.execute("DELETE from dados WHERE id = ?", (__id,))
-            self.banco.commit()
+            cursor.execute("DELETE from dados WHERE id = ?", (__id,))
+            banco.commit()
+            banco.close()
         except sqlite3.Error:
+            banco.close()
             pass
 
     # Obtem o ultimo id para paginação
     def obter_ultimo_id(self, nome=None, email=None, data=None):
+        banco = sqlite3.connect(self.db_name)
+        cursor = banco.cursor()
+
         # Inicia a consulta SQL
         consulta_sql = "SELECT id FROM dados WHERE 1=1"
 
@@ -100,13 +119,15 @@ class Banco:
         valores = [valor for valor in (nome, email, data) if valor is not None]
 
         # Executa a consulta SQL
-        self.cursor.execute(consulta_sql, valores)
+        cursor.execute(consulta_sql, valores)
 
         # Recupera o resultado
-        resultado = self.cursor.fetchone()
+        resultado = cursor.fetchone()
 
         # O resultado é uma tupla, então pegamos o primeiro elemento
         ultimo_id = resultado[0] if resultado else None
+
+        banco.close()
 
         return ultimo_id
 
